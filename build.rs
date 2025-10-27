53ccd98a599b86a6b254e38aa8c4115a166f822f
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-27 11:16:43 -0600

    copilot is thinking

--- build.rs
+++ build.rs
@@ -11,9 +11,13 @@ fn main() {
 
 #[cfg(not(target_os = "linux"))]
 fn main() {
+    let source_files = vec!["./src/c/co5_dl_double_osx.c",
+        "./src/c/co5_dl_losses_double_osx.c",
+        "./src/c/co5_dl_float_osx.c",
+        "./src/c/co5_dl_losses_float_osx.c"];
     println!("cargo:rerun-if-changed=./src/c/*");
     cc::Build::new()
-                .files(vec!["./src/c/co5_dl_double_osx.c", "./src/c/co5_dl_float_osx.c"])
+                .files(source_files)
                 .include("./src/c/headers")
                 .flag("-w")
                 .flag("-g")
